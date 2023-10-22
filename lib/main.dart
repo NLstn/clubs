881cb581e28de483a24616cb9713bfd10e8e3131
Niklas Lahnstein <niklas.lahnstein@gmail.com>
2023-10-22 12:34:56 +0200

    Core: Initialize Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:clubs/firebase_options.dart';
 import 'package:clubs/screens/core/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl_standalone.dart';
@@ -6,6 +8,9 @@ import 'package:intl/intl_standalone.dart';
 void main() async {
   var locale = await findSystemLocale();
   await initializeDateFormatting(locale, null);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
