6d7114b5a9c1fc5db736ca858e93abd89cf2166c
Niklas Lahnstein <niklas.lahnstein@gmail.com>
2023-11-01 15:03:13 +0100

    Setup Firebase Emulators for debug development

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:clubs/firebase_options.dart';
 import 'package:clubs/screens/core/authentication/auth_gate_screen.dart';
 import 'package:clubs/services/auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl_standalone.dart';
@@ -13,6 +16,13 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  if (kDebugMode) {
+    FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+    FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+    print('Running in debug mode');
+  }
+
   runApp(
     ChangeNotifierProvider(
       create: (context) => AuthService(),
