d9ed30d57256441cb8d8db51da990f787db4eb45
Niklas Lahnstein <niklas.lahnstein@gmail.com>
2023-11-24 00:21:05 +0100

    Change app title to clubs

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Clubs',
       home: AuthGateScreen(),
       debugShowCheckedModeBanner: false,
     );
