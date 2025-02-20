96351aa8be429c654040e16ddd7e13f178c02c44
Niklas Lahnstein <niklas.lahnstein@outlook.com>
2025-02-20 12:54:30 +0100

    feat: add DELETE method support in CORS middleware and implement member deletion in ClubDetails component

--- backend/handlers/middlewares.go
+++ backend/handlers/middlewares.go
@@ -19,7 +19,7 @@ func (rw *responseWriter) WriteHeader(code int) {
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		// Handle preflight request
